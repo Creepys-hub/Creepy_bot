e9bc9acd10df2fd6d5bfe0e5bd4094aa702d228e
Creep53 <creepminecraft53@gmail.com>
2019-06-28 11:26:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -26,16 +26,7 @@ bot.on('message', function (message){
 
 
 bot.on('message', msg => {
-    if (msg.content === "bonjour"){
-        msg.reply("Heureux de te revoir parmis nous.")
-    }
-    if (msg.content.match(/salut/i)) {
-            msg.reply('Je suis d\'accord avec toi.')
-    }
-    if (msg.content === prefix + "site"){
-        msg.channel.send("https://alexpgm.000webhostapp.com/")
-        console.log("Une personne a demandé pour aller sur ton site.")
-    }
+ 
 
 });
 
